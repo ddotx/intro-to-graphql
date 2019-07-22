4d612cdd0237f3dfbb3d478f5c58189edd6d5038
ddotx <domemacintosh@gmail.com>
2019-07-23 00:02:25 +0700

    update before exercise

--- src/server.js
+++ src/server.js
@@ -6,20 +6,64 @@ import { connect } from './db'
 import product from './types/product/product.resolvers'
 import coupon from './types/coupon/coupon.resolvers'
 import user from './types/user/user.resolvers'
+import { handleLegacyOptions } from 'apollo-engine-reporting/dist/agent';
 
 const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
+
+  //TODO=== Create GraphQL schemas by Schema Definition Language (SDL)
+  //?== 1-Scalar Types => String, Int, Float, Boolean, ID
+  //?== 2-Object Types => Custom shapes with fields | describe any arguments and/or validations
+  //?== 3-Query Types => type Query {READ:...}
+  //?== 4-Mutation Types => the intent of mutation the DB (CRUD)
   const rootSchema = `
+  type Cat {
+    name: String
+    age: Int!
+    bestFriend: Cat
+  }
+
+  input CatInput {
+    name: String
+    age: Int!
+    bestFriend: Cat
+  }
+
+  type Query {
+    myCat: Cat
+    hello: String
+  }
+
+  type Mutation {
+    newCat(input: CatInput!): Cat!
+  }
+
     schema {
       query: Query
     }
   `
+
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: {
+      Query: {
+        myCat(){
+          return {
+            name: 'Garfield',
+            bestFriend: {
+              name: 'Cony'
+            }
+            
+          }
+        },
+        hello(){
+          return "Hi i'm GraphQL"
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }

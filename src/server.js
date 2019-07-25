64d1938b9ca9b0df772433d3b144f776fb86b64f
ddotx <domemacintosh@gmail.com>
2019-07-25 23:29:10 +0700

    updated before resolvers exercise

--- src/server.js
+++ src/server.js
@@ -11,16 +11,68 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Cat {
+      owner: Owner!
+      name: String!
+      age: Int
+    }
+    type Owner {
+      name: String
+      cat: Cat!
+    }
+    type Query {
+      cat(name: String!): Cat!
+      owner(name: String!): Owner!
+    }
     schema {
       query: Query
-      mutation: Mutation
     }
   `
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    typeDefs: [rootSchema],
+    // resolvers: merge({}, product, coupon, user),
+    resolvers:{
+      // Query:{
+      //   cat(_, args){
+      //     console.log('in cat resolver')
+      //     return {name: args.name, age: 3, owner: {}}
+      //   },
+      //   owner(_, args){
+      //     console.log('in owner resolver')
+      //     return {name: args.name, cat: {}}
+      //   }
+      // }
+      //TODO: Field Type Resolvers
+      Query:{
+        cat(_, args){
+          return {}
+        },
+        owner(_, args){
+          return {}
+        }
+      },
+      Cat:{
+        name() {
+          return 'Daryl'
+        },
+        age(){
+          return 2
+        },
+        owner(){
+          return {}
+        }
+      },
+      Owner:{
+        name(){
+          return 'Scott'
+        },
+        cat(){
+          return {}
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
